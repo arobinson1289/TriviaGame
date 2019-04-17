6e98958b7964f2c6af90e2baeb21a4b9f77d4ae0
Andrew Robinson <arobinson1289@gmail.com>
2019-04-16 21:54:38 -0400

    changes

--- assets/javascript/ap.js
+++ assets/javascript/ap.js
@@ -1,22 +1,23 @@
-$(document).ready(function (){
-//Javascript Code Will Begin Here!
-var questions = [
-    {
-      question: 'Question 1',
-      answer: 'Answer 1',
-      choices: ['Answer 1', "Answer 2", "Answer 3"],
+$(document).ready(function () {
+  //Javascript Code Will Begin Here!
+  clearInterval(intervalID);
+
+  var questions = [{
+      question: 'What is the capitol of Arizona?',
+      answer: 'Pheonix',
+      choices: ['Phoenix', "Tempe", "Scottsdale"],
       userAnswer: ""
     },
     {
-      question: 'Question 2',
-      answer: 'Answer 2',
-      choices: ['Answer 1', "Answer 2", "Answer 3"],
+      question: 'What is the capital of New York',
+      answer: 'Albany',
+      choices: ['Albany', "Hudson", "New York City"],
       userAnswer: ""
     },
     {
-      question: 'Question 3',
-      answer: 'Answer 3',
-      choices: ['Answer 1', "Answer 2", "Answer 3"],
+      question: 'What is the capital of New Jersey',
+      answer: 'Trenton',
+      choices: ['Newark', "Asbury Park", "Trenton"],
       userAnswer: ""
     }
   ];
@@ -24,79 +25,61 @@ var questions = [
   var correct = 0;
 
   //Set Var For Interval Timer
-  var timer = 10;
+  var timer = 30;
   var intervalID;
   var clockTicking = false;
+  var correct = 0;
+  var incorrect = 0;
 
-  function decTimer(){
+  //Function For Interval Timer//
+  function decTimer() {
     clearInterval(intervalID);
 
-    intervalID = setInterval(function(){
+    intervalID = setInterval(function () {
       timer--;
       $("#timer").html(timer);
-      
-      if(timer === 0){
-        clearInterval(intervalID)
-        $("#alertTImer").html("<h1>TIMES UP</h1>");
+      if (timer === 0) {
+        clearInterval(intervalID);
+        $("#timer").html('<h2>Time is up!</h2>');
+
       }
-    },1*1000);
-  }
-
-  function buttonClick (){
-    clockTicking = !clockTicking;
-
-    if (clockTicking) {
-      clearInterval(intervalID);
-    } else {
-        decTimer();
-    }
-
+    }, 1 * 1000);
   };
 
-  $("#button").on("click",buttonClick);
-  $(document).on("click","#btn2", function (){
-    alert("this worked MF")
-  })
+  var userScore = 0;
 
-  decTimer()
-  
   // function to print all questions to page
   function renderQuestions() {
     // clear out form
     $("#quiz-form").empty();
 
-  
-
     // Loop through questions array
     questions.forEach(function (question, index) {
       // create div to hold question
       var $question = $("<div>").addClass("form-group");
-        // <div class="form-group"></div>
-      
+      // <div class="form-group"></div>
+
       // add question to div
       var $label = $("<h4>")
         .text(question.question)
         .appendTo($question);
-          /*
-            <div class="form-group"> 
-              <h4>Question 1</h4> 
-            </div>
-          */ 
-
+      /*
+        <div class="form-group"> 
+          <h4>Question 1</h4> 
+        </div>
+      */
       // shuffle choices
-      question.choices = question.choices.sort(function() {
+      question.choices = question.choices.sort(function () {
         return .5 - Math.random();
       });
-
       // create a loop to iterate through question's choices and create radio buttons for each one
       for (var i = 0; i < question.choices.length; i++) {
         // create a div for choice and add bootstrap classes
         var $choice = $('<div>');
         $choice.addClass('form-check form-check-inline');
-        
+
         // create an input tag for the radio button
         var $radio = $('<input>');
-
         // add attributes to provide the answer choice
         // the "name" attribute is super important, all radio buttons per question need to have the same "name" so they know which question it applies to
         $radio
@@ -107,41 +90,64 @@ var questions = [
             class: "form-check-input"
           })
           .appendTo($choice);
-        
+
         // create label to actually print the choice to the page
         var $choiceLabel = $('<label>');
         $choiceLabel
           .text(question.choices[i])
           .addClass('form-check-label')
           .appendTo($choice);
-        
+
         // add whole radio button choice to question
         $choice.appendTo($question);
       }
       // when done making all of the choices, add whole question to the page
       $("#quiz-form").append($question);
-
     });
+    var $button = $('<button>');
+    $button.attr("id", "submit-button");
+    $button.text("Submit");
+    $("#quiz-form").append($button);
   }
 
- 
-  // create on "change" listener for all radio buttons but bind them to quiz-form since it's permanently on the page
-  $("#quiz-form").on("change", ".form-check-input", function() {
-    console.log(this);
+  // create a loop to iterate through question 
+
+  function checkAnswers() {
+    for (var i = 0; i < questions.length; i++) {
+      if (questions[i].userAnswer === questions[i].answer) {
+        correct++;
+      } else {
+        incorrect++;
+        console.log("hello");
+      }
+    }
+  }
+
+  function results (){
     
-    // GET question index out of "name" attribute so we know what question you answered
-    var questionIndex = $(this).attr("name");
+  }
 
-    console.log(questions[questionIndex]);
+  $(document).on("click", "#submit-button", function () {
+    event.preventDefault();
+    checkAnswers();
+    console.log(correct, incorrect);
+    $("#quiz-form").html("<h3>Total Correct: "+ correct + "</h3><br><h3>Total Incorrect: " + incorrect);
+    clearInterval(intervalID);
 
-    // get value out of radio button you selected
-    var answer = $(this).val();
+  })
 
-    // set answer to question's userAnswer property
-    questions[questionIndex].userAnswer = answer;
-    
-  });
+  //Start New Game WHen New Game Button Is Clicked//
+  function startNewGame() {
+    event.preventDefault();
+    decTimer();
+    $("#startButton").html(" ");
+    renderQuestions();
+    alert("you started");
 
-  renderQuestions();
+  }
+
+  $("#startButton").on("click", function () {
+    startNewGame();
+  })
 
 })
